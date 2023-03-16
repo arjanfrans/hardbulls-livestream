81e3f3c727f4f61db89cf49e3ac35b139e0d2ae3
Arjan Frans <arjan.frans@fusonic.net>
2023-03-16 17:50:54 +0000

    rename out

--- src/baseball/Counts.tsx
+++ src/baseball/Counts.tsx
@@ -7,7 +7,7 @@ export const Counts = ({strikes, balls, outs}: Props) => {
     return (
         <div className="counts-container">
             <div className="counts-top"><span>{balls} - {strikes}</span></div>
-            <div className="counts-bottom"><span>{outs} outs</span></div>
+            <div className="counts-bottom"><span>{outs} out</span></div>
         </div>
     )
 }
